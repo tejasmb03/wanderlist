4b1d346f9e5efe104d6cb9ebc24e7f27a2f8523e
tejas m bharadwaj <tejasmb2003@gmail.com>
2025-07-11 14:40:53 +0530

    Update API baseURL to production

--- frontend/my-wander-app/src/services/api.js
+++ frontend/my-wander-app/src/services/api.js
@@ -1,4 +1,8 @@
 // --- src/services/api.js ---
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000" });
+
+const API = axios.create({
+  baseURL: "https://wanderlist-acsk.onrender.com", // ← your Render backend URL
+});
+
 export default API;
